6cc8a8815117b2a2b2e5ea856450181ac1c2a919
Elkin Humberto Chaparro Franklin <elkinchaparro@gmail.com>
2025-02-21 21:28:32 -0500

    Implement main application logic and add product management features

--- executable.java
+++ executable.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class executable {
-public static void main(String[] args) {
-    
-}
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        boolean exit = false;
+        showMenu.welcome();
+        while (!exit) {
+
+            var option = showMenu.principalMenu(null);
+
+            try {
+                option = Integer.parseInt(scanner.nextLine());
+                // switch para seleccionar el tipo de emergencia
+                switch (option) {
+                    case 1:
+                    // Seleccionar producto
+                    añadirproducto.agregarAlCarrito(scanner);
+                        break;
+                    case 2:
+                    // Ver productos en el carrito
+                        break;
+                    case 3:
+                    // Eliminar producto del carrito
+                        break;
+                    case 4:
+                    // Ver total a pagar
+                        break;
+                    case 5:
+                    // Salir
+                        exit = true;
+                        break;
+                    default:
+                        throw new NullPointerException();
+                }
+            } // Se captura la excepción en caso de que la opción no sea un número
+            catch (NumberFormatException e) {
+                showMenu.serrMenu();
+                break;
+            } // Se captura la excepción en caso de que la opción sea nula
+            catch (NullPointerException e) {
+                showMenu.serrMenu();
+                break;
+            }
+        }
+
+    }
 }
